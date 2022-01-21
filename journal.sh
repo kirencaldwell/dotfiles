103fd739a43a2c2ce7f7c391ebf6cc731385f2d3
Kiren Caldwell <kcaldwell@zoox.com>
2022-01-21 22:13:53 +0000

    adding search feature

--- journal.sh
+++ journal.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-#now=$(date +"%T")
-now=$(date)
-note_to_print=$*
-echo "$now: $note_to_print" >> ~/.my_journal.txt
-echo "$now: $note_to_print"
+take_note () {
+  NOW=$(date)
+  NOTE="$*"
+  echo "$NOW: $NOTE" >> ~/.my_journal.txt
+  echo "$NOW: $NOTE"
+}
+
+OPT="${1}"
+case ${OPT} in
+  -s | search) SEARCH="${2}"
+    echo "Searching for $SEARCH"
+    grep $SEARCH ~/.my_journal.txt --color
+    ;;
+  *)
+    take_note "$*"
+    ;;
+esac
